0359804b7aaa0a1722b72e58559703cb183330a6
algafix <algafix@gmail.com>
2023-03-15 23:17:06 +0100

    interacts with the leds on the tiny board

---
+++ main_led.py
@@ -0,0 +1,40 @@
+from machine import Pin
+from time import sleep_us,sleep_ms
+
+# LED Code specific for the tiny 2040 
+# Define led components
+red = Pin(18, Pin.OUT)
+green = Pin(19, Pin.OUT)
+blue = Pin(20, Pin.OUT)
+
+# Turn off led - Active on low
+red.value(1)
+green.value(1)
+blue.value(1)
+
+data = Pin(29, Pin.OUT)
+
+signal = [1, 0, 1, 0, 1, 0, 1, 0, 1, 0, 1, 0, 1, 0, 1, 0, 1, 0, 1, 0, 1, 0, 1, 0, 1, 0, 0, 0, 1, 0, 1, 0, 1, 1, 0, 0, 1, 0, 1, 1, 0, 0, 1, 1, 0, 0, 1, 0, 1, 1, 0, 0, 1, 1, 0, 0, 1, 1, 0, 0, 1, 1, 0, 0, 1, 1, 0, 0, 1, 0, 1, 1, 0, 1, 0, 0, 1, 1, 0, 1, 0, 0, 1, 0, 1, 1, 0, 1, 0, 1, 0, 0, 1, 0, 1, 0, 1, 1, 0, 1, 0, 0, 1, 1, 0, 1, 0, 0, 1, 1, 0, 0, 1, 0, 1, 0, 1, 0, 1, 1, 0, 1, 0, 0, 1, 0, 1, 1, 0, 0, 0, 1, 0, 1, 0, 1, 1, 0, 0, 1, 0, 1, 1, 0, 0, 1, 1, 0, 0, 1, 0, 1, 1, 0, 0, 1, 1, 0, 0, 1, 1, 0, 0, 1, 1, 0, 0, 1, 1, 0, 0, 1, 0, 1, 1, 0, 1, 0, 0, 1, 1, 0, 1, 0, 0, 1, 0, 1, 1, 0, 1, 0, 1, 0, 0, 1, 0, 1, 0, 1, 1, 0, 1, 0, 0, 1, 1, 0, 1, 0, 0, 1, 1, 0, 0, 1, 0, 1, 0, 1, 0, 1, 1, 0, 1, 0, 0, 1, 0, 1, 1, 0, 0, 0, 1, 0, 1, 0, 1, 1, 0, 0, 1, 0, 1, 1, 0, 0, 1, 1, 0, 0, 1, 0, 1, 1, 0, 0, 1, 1, 0, 0, 1, 1, 0, 0, 1, 1, 0, 0, 1, 1, 0, 0, 1, 0, 1, 1, 0, 1, 0, 0, 1, 1, 0, 1, 0, 0, 1, 0, 1, 1, 0, 1, 0, 1, 0, 0, 1, 0, 1, 0, 1, 1, 0, 1, 0, 0, 1, 1, 0, 1, 0, 0, 1, 1, 0, 0, 1, 0, 1, 0, 1, 0, 1, 1, 0, 1, 0, 0, 1, 0, 1]
+sample_time = 384 # Should be 400us, but we need to account for the time it takes to the python code to execute.
+repeat = 10
+
+# Evey 3 second
+while True:
+    # X times the same signal
+    for r in range(repeat):
+        for i in signal:
+            data.value(i)
+            sleep_us(sample_time)
+        data.value(0)
+        sleep_us(5*sample_time)
+        red.toggle()
+    red.value(1)
+    
+    blue.value(0)
+    sleep_ms(500)
+    blue.value(1)
+    sleep_ms(500)
+    blue.value(0)
+    sleep_ms(500)
+    blue.value(1)
+    sleep_ms(500)
